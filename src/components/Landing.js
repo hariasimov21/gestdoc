01dd40025eb7c4d4461493cff3843a9695c64823
jaime <james18bt@gmail.com>
2021-04-16 17:27:59 -0400

    font awesome paquetes instalado

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-<<<<<<< HEAD
-=======
-
->>>>>>> 56e4da36fdf67a26e44da4a8c9bd3bab9c34bb04
 import Navigation from "./Navigation";
 
 export default class Landing extends Component {
