05fffbfa1995d22a2bfb17635f1be73df849c0fb
jaime <james18bt@gmail.com>
2021-04-16 17:20:08 -0400

    cambios realizados2

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,10 +2,6 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-<<<<<<< HEAD
-
-=======
->>>>>>> f4a881a324761d64d5a215758f3303871c9d01b9
 import Navigation from "./Navigation";
 
 export default class Landing extends Component {
@@ -14,17 +10,11 @@ export default class Landing extends Component {
   };
 
   render() {
-<<<<<<< HEAD
     return (
       <>
         <Navigation />
-        <div className="container">
-          <FontAwesomeIcon icon={faCoffee} />
-        </div>
+        <FontAwesomeIcon icon={faCoffee} />
       </>
     );
-=======
-    return <Navigation />;
->>>>>>> f4a881a324761d64d5a215758f3303871c9d01b9
   }
 }
