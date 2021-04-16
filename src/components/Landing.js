4922b9837f4830a54ce01c33919c23538b8b61a0
jaime <james18bt@gmail.com>
2021-04-16 17:09:36 -0400

    font awesome

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-<<<<<<< HEAD
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
 
-=======
->>>>>>> 7d0efff6f8fabe7a4dc2aec6b390519f3de91eb2
 import Navigation from "./Navigation";
 
 export default class Landing extends Component {
@@ -14,7 +11,6 @@ export default class Landing extends Component {
   };
 
   render() {
-<<<<<<< HEAD
     return (
       <>
         <Navigation />
@@ -23,8 +19,5 @@ export default class Landing extends Component {
         </div>
       </>
     );
-=======
-    return <Navigation />;
->>>>>>> 7d0efff6f8fabe7a4dc2aec6b390519f3de91eb2
   }
 }
