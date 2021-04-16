56e4da36fdf67a26e44da4a8c9bd3bab9c34bb04
juanMAA <juan.mansilla@Egt.cl>
2021-04-16 17:11:50 -0400

    Cambios

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -2,10 +2,7 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCoffee } from "@fortawesome/free-solid-svg-icons";
-<<<<<<< HEAD
 
-=======
->>>>>>> f4a881a324761d64d5a215758f3303871c9d01b9
 import Navigation from "./Navigation";
 
 export default class Landing extends Component {
@@ -14,7 +11,6 @@ export default class Landing extends Component {
   };
 
   render() {
-<<<<<<< HEAD
     return (
       <>
         <Navigation />
@@ -23,8 +19,5 @@ export default class Landing extends Component {
         </div>
       </>
     );
-=======
-    return <Navigation />;
->>>>>>> f4a881a324761d64d5a215758f3303871c9d01b9
   }
 }
